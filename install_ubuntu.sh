872e2284df46c388c8568885787f45c0c6aaf792
Mikhail Vasilyev <mickvav@fb.com>
2021-03-23 08:46:52 +0000

    Bugbixes.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,4 +1,4 @@
-/bin/bash
+#!/bin/bash
 
 vmname=ubuntu20
 vdipath=`pwd`/.vdi
@@ -7,7 +7,7 @@ vm_user=`whoami`
 echo "Enter password for ${vm_user} on future vm:"
 read -s vm_password
 mkdir "${vdipath}" || echo "VDI path already exists"
-vdisize=10240
+vdisize=20480
 ubuntu_image=ubuntu-20.04.2.0-desktop-amd64.iso
 ubuntu_version=20.04
 isopath=`pwd`/.iso/ubuntu/${ubuntu_version}
@@ -59,5 +59,6 @@ echo "apt-get install openssh-server git"
 echo "echo -ne \"PubkeyAuthentication yes\\nAuthorizedKeysFile	.ssh/authorized_keys .ssh/authorized_keys2\\n\" >> /etc/ssh/sshd_config.d/allow_public_key"
 echo "/etc/init.d/ssh restart"
 echo "Press enter to continue when this will be done"
+read A
 ssh-copy-id -p 2022 ${vm_user}@127.0.0.1
 ssh -p 2022 ${vm_user}@127.0.0.1 "git clone https://github.com/mickvav/devenvs_vbox.git && cd devenvs_vbox && bash ./postinstall.sh"
