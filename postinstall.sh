35e551bd3dcc27ca6d392cde923c8221b7d047eb
Mikhail Vasilyev <mickvav@fb.com>
2021-03-26 23:53:06 +0000

    Fixes + arduino-cli

--- postinstall.sh
+++ postinstall.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 echo "Post-install..."
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install vim
-#wget https://downloads.arduino.cc/arduino-1.8.13-linux64.tar.xz
-#tar -xvJf arduino-1.8.13-linux64.tar.xz
+sudo -S apt-get update
+sudo -S apt-get upgrade
+sudo -S apt-get install vim
+wd=`pwd`
+wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
+tar -xvJf arduino-cli_latest_Linux_64bit.tar.gz
+wget https://downloads.arduino.cc/arduino-1.8.13-linux64.tar.xz
+tar -xvJf arduino-1.8.13-linux64.tar.xz
 cd arduino-1.8.13
 ./install.sh
-sudo usermod -a -G dialout $USER && \
-sudo apt-get install git python3-pip && \
-sudo pip3 install pyserial && \
+sudo -S usermod -a -G dialout $USER && \
+sudo -S apt-get install git python3-pip curl black && \
+sudo -S pip3 install pyserial && \
 mkdir -p hardware/espressif && \
 cd hardware/espressif && \
 git clone https://github.com/espressif/arduino-esp32.git esp32 && \
